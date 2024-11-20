140f6172c493c56ac59d177b69051a5647c087a4
César Borba <126433881+cesar-borba@users.noreply.github.com>
2024-11-20 05:33:59 -0300

    Adding bee_2449.cpp

---
+++ cpp/bee_2449.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int n, m;
+
+    cin >> n >> m;
+
+   vector<int>vetor;
+
+    for (int i = 0; i< n; i++) {
+        int a;
+        cin >> a;
+        vetor.push_back(a);
+    }
+    
+    int movimentos = 0;
+
+    for (int i=0; i<vetor.size(); i++) {
+        int diferenca;
+        if (vetor[i]!=m) {
+            diferenca = m -vetor[i];
+            vetor[i]+=diferenca;
+            vetor[i+1]+=diferenca;
+            movimentos+=abs(diferenca);
+        }
+    }
+
+    cout << movimentos << endl;
+}
