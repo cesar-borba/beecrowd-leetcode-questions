216b25c44e30c3f2cfb129389483a552ccaed6e2
César Borba <cesarmartins350@gmail.com>
2025-01-11 02:58:44 -0300

    Adding bee_2381.cpp

---
+++ cpp/bee_2316.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+//Nome: Autorama
+//Assunto: Desconhecido
+//Competição/criador: OBI 2006
+//https://judge.beecrowd.com/pt/problems/view/2316
+
+using namespace std;
+
+int main() {
+
+    //k-> número de postos
+    //n-> num carrinhos
+    //m-> num leituras
+    int k, n, m;
+
+    cin >> k >> n >> m;
+
+    return 0;
+
+}
