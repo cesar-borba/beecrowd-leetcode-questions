20d1302c5da014e2f3a2cab4e19c569391c239ab
César Borba <cesarmartins350@gmail.com>
2025-01-11 13:14:04 -0300

    Adding bee_1000.cpp

---
+++ cpp/bee_1000.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+
+//Nome: Hello World!
+//Assunto: Hello World!
+//Competição/criador: Jean Bez, beecrowd Brasil
+//https://judge.beecrowd.com/pt/problems/view/1000
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
