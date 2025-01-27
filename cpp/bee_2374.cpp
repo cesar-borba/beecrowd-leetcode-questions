ad816a587dc44d3a66d681df3fe25c2cbecc2024
César Borba <cesarmartins350@gmail.com>
2025-01-27 00:54:40 -0300

    Adding bee_2374.cpp

---
+++ cpp/bee_2374.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Pneu
+//Assunto: Desconhecido
+//Competição/criador: OBI 2010
+//https://judge.beecrowd.com/pt/problems/view/2374
+
+using namespace std;
+
+int main() { _
+
+    int pd, pl;
+    
+    cin >> pd >> pl;
+
+    cout << pd-pl << endl;
+
+    return 0;
+}
