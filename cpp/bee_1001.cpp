5cd718e7133855007c5a897f49937741c5e5c3b7
César Borba <cesarmartins350@gmail.com>
2025-01-26 22:55:43 -0300

    Adding bee_1001.cpp

---
+++ cpp/bee_1001.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Extremamente básico
+//Assunto: Sequencial
+//Competição/criador: Neilor Tonin, URI Brasil
+//https://judge.beecrowd.com/pt/problems/view/1001
+
+using namespace std;
+
+int main() { _
+
+    int a, b;
+
+    cin >> a >> b;
+
+    cout << "X = " << a+b << endl;
+
+    return 0;
+}
