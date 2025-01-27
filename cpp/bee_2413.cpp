1cf3aef6398efa607bcd565d5d74408da5ad50d0
César Borba <cesarmartins350@gmail.com>
2025-01-27 00:49:38 -0300

    Adding bee_2413.cpp

---
+++ cpp/bee_2413.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Busca na internet
+//Assunto: Ad hoc
+//Competição/criador: OBI 2012
+//https://judge.beecrowd.com/pt/problems/view/2413
+
+using namespace std;
+
+int main() { _
+
+    int t;
+
+    cin >> t;
+
+    cout << t*4 << endl;
+
+    return 0;
+}
