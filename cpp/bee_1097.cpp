a9454d9ab8078f5220e798765b0632f1083f8b22
César Borba <cesarmartins350@gmail.com>
2025-01-27 01:43:38 -0300

    Adding bee_1097.cpp

---
+++ cpp/bee_1097.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Sequencia IJ 3
+//Assunto: Repetição
+//Competição/criador: Neilor Tonin, URI Brasil
+//https://judge.beecrowd.com/pt/problems/view/1097
+
+using namespace std;
+
+int main() { _
+
+    int i=1, j=7;
+
+    while (i<=9) {
+        for (int cont = 0; cont < 3; cont++)
+        {
+            cout << "I=" << i << " J=" << j << endl;
+            j-=1;
+        }
+        i+=2;
+        j+=5;
+    }
+
+    return 0;
+}
