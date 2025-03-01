9c7d8c5dd22754517f6db2fcaac4966154448573
César Borba <cesarmartins350@gmail.com>
2025-03-01 00:00:06 -0300

    Adding bee_1059.cpp

---
+++ cpp/bee_1059.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Números Pares
+//Assunto: Repetição
+//Competição/criador: Neilor Tonin, URI Brasil
+//https://judge.beecrowd.com/pt/problems/view/1059
+
+using namespace std;
+
+int main() { _
+    
+    for (int i = 1; i < 100; i++)
+    {
+        i++;
+        cout << i << endl;
+    }
+    
+
+    return 0;
+}
