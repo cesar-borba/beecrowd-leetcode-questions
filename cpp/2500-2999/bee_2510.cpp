9f154fdce1eb6f48f1bee4fb8fcd6561029bff78
César Borba <cesarmartins350@gmail.com>
2025-05-07 23:48:06 -0300

    Adding bee_2510.cpp

---
+++ cpp/2500-2999/bee_2510.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+#define FOR(i, a, b) for (int i = a; i <= b; ++i)
+#define RFOR(i, b, a) for (int i = b; i >= a; --i)
+#define REP(i, N) for (int i = 0; i < N; ++i)
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+//Nome: Batmain
+//Assunto: Desconhecido
+//Competição/criador: Guilherme Miranda, UNOESTE BR Brazil
+//https://judge.beecrowd.com/pt/problems/view/2510
+
+using namespace std;
+
+int main() { _
+
+    int n;
+
+    cin >> n;
+
+    for(int i=0; i<n; i++) {
+        string a;
+        cin >> a;
+    }
+
+    while(n--) cout << "Y" << endl;
+
+    return 0;
+}
