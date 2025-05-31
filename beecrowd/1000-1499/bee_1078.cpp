f1fb1883c1d810d6b433495ab30fe6f9c36081f7
César Borba <cesarmartins350@gmail.com>
2025-05-31 05:05:42 -0300

    Adding bee_1078.cpp

---
+++ beecrowd/1000-1499/bee_1078.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define print(x) cout << x << "\n"
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+//Nome: Tabuada
+//Assunto: Repetição
+//Competição/criador: Neilor Tonin, URI Brasil
+//Nível: 1
+//https://judge.beecrowd.com/pt/problems/view/1078
+
+int main() { _
+
+    int n; 
+    cin >> n;
+
+    for (int i = 1; i <= 10; i++) cout << i << " x " << n << " = " << i*n << endl;
+    
+    return 0;
+}
