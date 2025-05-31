d939b47a30ff1cee4b34cbe480a15f331e539d2a
César Borba <cesarmartins350@gmail.com>
2025-05-31 06:17:50 -0300

    Adding bee_1116.cpp

---
+++ beecrowd/1000-1499/bee_1116.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define dbg(x) cout << #x << " = " << x << endl
+#define print(x) cout << x << "\n"
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+//Nome: Dividindo X por Y
+//Assunto: Repetição 
+//Competição/criador: Neilor Tonin, URI  Brasil
+//Nível: 1
+//https://judge.beecrowd.com/pt/problems/view/1116
+
+int main() { _
+
+    int n;
+    cin >> n;
+    while(n--) {
+        double a, b;
+        cin >> a >> b;
+        if(b==0) cout << "divisao impossivel\n";
+        else cout << fixed << setprecision(1) << a/b << endl;
+    }
+
+    return 0;
+}
